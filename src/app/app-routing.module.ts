1e75176ccea19d1f785e80eb5012381ff0f04e3a
unknown <himadriapurwa1310@gmail.com>
2023-07-28 14:52:46 +0530

    cart dashboard invoice products user-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { ProductsComponent } from './products/products.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'invoice', component:InvoiceComponent
+},
+{path:'products', component: ProductsComponent},
+{path:'user-list', component: UserListComponent},{path:'dashboard', component: DashboardComponent},
+{path:'cart', component:CartComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvoiceComponent } from './invoice/invoice.component';
+import { ProductsComponent } from './products/products.component';
+import { UserListComponent } from './user-list/user-list.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CartComponent } from './cart/cart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoiceComponent,
+    ProductsComponent,
+    UserListComponent,
+    
+    DashboardComponent,
+         CartComponent,
+  
   ],
   imports: [
     BrowserModule,
